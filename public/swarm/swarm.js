509d2bc7fb7ebf80ca375639df2d132ab9d5a519
Joel Dietz <jdietz+github@gmail.com>
2015-09-29 20:37:45 -0700

    deployed and working

--- public/swarm/swarm.js
+++ public/swarm/swarm.js
@@ -4,11 +4,11 @@ angular.module('swarm', ['ngRoute'])
 
 .config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/swarm', {
-    templateUrl: 'public/swarm/swarm.html',
+    templateUrl: './swarm/swarm.html',
     controller: 'SwarmCtrl'
   });
 }])
 
 .controller('SwarmCtrl', [function() {
-    
+
 }]);
